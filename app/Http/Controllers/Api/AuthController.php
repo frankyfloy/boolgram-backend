669df8e4c7e14b838c924d074d5b41e3b74141b7
Francesco Sica <fsica171@gmail.com>
2021-07-19 17:06:52 +0200

    OAuth2 implementation with laravel passport
    Token issuance to make API calls to routes with Api namespace.
    Custom controller and management of login, logout and signup routes for API requests.
    Use of Laravel passport for token creation at login and revocation at logout.

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\User;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request){
+
+        $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+        $credentials = request(['email', 'password']);
+
+        if(!Auth::attempt($credentials)){
+            return response()->json([
+                'message'=> 'Invalid email or password'
+            ], 401);
+        }
+
+        $user = $request->user();
+
+        $token = $user->createToken('Access Token');
+
+        $user->access_token = $token->accessToken;
+
+        return response()->json([
+            "user"=>$user
+        ], 200);
+    }
+
+    public function signup(Request $request){
+        $request->validate([
+            'name' => 'required|string|max:20',
+            'surname' => 'required|string|max:30',
+            'date_of_birth' => 'required|date_format:"Y-m-d"',
+            'email' => 'required|string|unique:users',
+            'password' => 'required|string|confirmed',
+        ]);
+
+        $user = new User([
+            'name' => $request->name,
+            'surname' => $request->surname,
+            'date_of_birth' => $request->date_of_birth,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        $user->save();
+
+        return response()->json([
+            "message" => "User registered successfully"
+        ],201);
+    }
+
+    public function logout(Request $request){
+        $request->user()->token()->revoke();
+        return response()->json([
+            "message" => "User logout successfully"
+        ],200);
+    }
+
+    public function index(){
+        echo "Stub Auth Token";
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::namespace('Api')->group(function () {
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::prefix('auth')->group(function () {
+        Route::post('login','AuthController@login');
+        Route::post('signup','AuthController@signup');
+    });
+
+    Route::group([
+        'middleware' => 'auth:api'
+    ],function(){
+        Route::get('Stub_Auth_Token','AuthController@index');
+        Route::post('logout','AuthController@logout');
+    });
+
 });
